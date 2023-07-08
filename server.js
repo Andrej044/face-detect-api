cb5a0b51c06d6194430632ab7617da6f5568ec0e
Andrii Lishchyna <andrej044@i.ua>
2023-07-07 21:51:10 -0400

    make server a responds

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+
+app.get('/', (req,res) => {
+  res.send("THIS IS WORKING!!!!");
+})
+
 app.listen(3000, () => {
   console.log("app is running  on port 3000");
 })
