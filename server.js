6408b78650cf499fef0235ddca661f24fb40765c
Andrii Lishchyna <andrej044@i.ua>
2023-07-20 14:59:14 -0400

    make a transaction between users and login tables

--- server.js
+++ server.js
@@ -67,21 +67,31 @@ app.post('/signin', (req,res) => {
 })
 
 app.post("/register", (req,res) => {
-  const {email, name, password} = req.body;
+    const {email, name, password} = req.body;
 
-    bcrypt.hash(password, null, null, function(err, hash) {
-      // Store hash in your password DB.
-    });
+    const hash = bcrypt.hashSync(password);
 
-    db('users')
-      .returning('*')
-      .insert({
-        email:email,
-        name:name,
-        joined: new Date()
-      })
-      .then(user => {
-        res.json(user[0]);
+      db.transaction(trx => {
+        trx.insert({
+          hash: hash,
+          email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              email:loginEmail[0].email,
+              name:name,
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0]);
+          })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
       })
       .catch(err => res.status(400).json("Can't register that user"))
   })
