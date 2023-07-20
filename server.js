95c3d0c173d754b28708a33b09b6c8d5b2face80
Andrii Lishchyna <andrej044@i.ua>
2023-07-20 14:13:39 -0400

    rebuild profile endpoint used knex

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post("/register", (req,res) => {
     bcrypt.hash(password, null, null, function(err, hash) {
       // Store hash in your password DB.
     });
-    
+
     db('users')
       .returning('*')
       .insert({
@@ -88,17 +88,19 @@ app.post("/register", (req,res) => {
 
 app.get("/profile/:id", (req, res) =>{
   const { id } = req.params;
-  let found = false;
 
-  database.users.forEach( user => {
-    if(user.id === id){
-      found = true;
-     return res.json(user);
-    } 
+  db.select('*').from('users').where({
+    id:id
   })
-  if(!found){
-      res.status(404).json("no such user");
-  }
+    .then(user => {
+      if(user.length){
+        res.json(user[0])
+      } else {
+        res.status(400).json('User not found')
+      }
+  })
+    .catch(err => res.status(400).json('Ups, not found that user'))
+
 })
 
 
