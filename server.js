1936088daf30e5ce4a13db012e76b2198cd1e3c8
Andrii Lishchyna <andrej044@i.ua>
2023-07-24 16:23:58 -0400

    implement signin handler

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const knex = require("knex");
 
 const register = require("./controllers/register"); 
 
+const signin = require("./controllers/signin");
+
 
 const db = knex({
   client: 'pg',
@@ -30,21 +32,8 @@ app.get('/', (req,res) => {
 });
 
 
-app.post('/signin', (req,res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if(isValid){
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('Unable to get user'))
-      } else res.status(400).json("Incorrect login or password")
-    })
-    .catch(err => res.status(400).json("Incorrect login or password"))
+app.post('/signin', (req, res)=>{
+  signin.signinHandler(req, res, db, bcrypt)
 })
 
 app.post("/register", (req,res)=>{
