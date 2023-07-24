0b2f12e81de42e58ca7dccc0ceed74d677be5615
Andrii Lishchyna <andrej044@i.ua>
2023-07-24 16:17:44 -0400

    import register handler and implement it

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 
+const register = require("./controllers/register"); 
+
 
 const db = knex({
   client: 'pg',
@@ -45,35 +47,9 @@ app.post('/signin', (req,res) => {
     .catch(err => res.status(400).json("Incorrect login or password"))
 })
 
-app.post("/register", (req,res) => {
-    const {email, name, password} = req.body;
-
-    const hash = bcrypt.hashSync(password);
-
-      db.transaction(trx => {
-        trx.insert({
-          hash: hash,
-          email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-          return trx('users')
-            .returning('*')
-            .insert({
-              email:loginEmail[0].email,
-              name:name,
-              joined: new Date()
-            })
-            .then(user => {
-              res.json(user[0]);
-          })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-      })
-      .catch(err => res.status(400).json("Can't register that user"))
-  })
+app.post("/register", (req,res)=>{
+  register.registerHandler(req, res, db, bcrypt);
+})
 
 app.get("/profile/:id", (req, res) =>{
   const { id } = req.params;
