a790e1680db7fbf8061f9cd80b1c6398ea257d0c
Andrii Lishchyna <andrej044@i.ua>
2023-08-01 18:32:15 -0400

    upd postgres connection

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ const image = require("./controllers/image");
 const db = knex({
   client: 'pg',
   connection: {
+    connectionString:process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized:false},
     host : process.env.DATABASE_HOST,
     port : 5432,
     user : process.env.DATABASE_USER,
     password : process.env.DATABASE_PW,
-    database : process.env.DATABASE_DB,
-    ssl: true
+    database : process.env.DATABASE_DB
   }
 });
 
