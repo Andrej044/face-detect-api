2fd083390ef93ec1b65052931509613c93fd281b
Andrii Lishchyna <andrej044@i.ua>
2023-08-01 20:01:07 -0400

    switch from require to import

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const bcrypt = require("bcrypt-nodejs");
-const cors = require("cors");
-const knex = require("knex");
+import express from "express";
+import bcrypt from "bcrypt-nodejs";
+import cors  from "cors";
+import knex  from "knex";
 import fetch from "node-fetch";
 
 
-const register = require("./controllers/register"); 
-const signin = require("./controllers/signin");
-const profile = require("./controllers/profile");
-const image = require("./controllers/image");
+import register from "./controllers/register"; 
+import signin from "./controllers/signin";
+import profile from "./controllers/profile";
+import image from "./controllers/image";
 
 const db = knex({
   client: 'pg',
